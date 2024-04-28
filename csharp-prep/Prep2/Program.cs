598630013233894670c3b300dbd3950c04d63c2f
Hioyuki <153518938+Hioyuki@users.noreply.github.com>
2024-04-28 14:37:34 +0900

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.Write("How is your score?");
+        string score = Console.ReadLine();
+
+        int X =int.Parse(score);
+        int y =70;
+
+        string letter="";
+
+        if( X >= 90)
+        {
+            letter="A";
+        }
+        else if (X >= 80)
+        {
+            letter="B";
+        }
+        else if (X >= 70)
+        {
+            letter="C";
+        }
+        else if (X >= 60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if(X > y)
+        {
+            Console.WriteLine("Congratulions!");
+        }
+        else if (X < y)
+        {
+            Console.WriteLine("Do your best next time!");
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
     }
 }
 
