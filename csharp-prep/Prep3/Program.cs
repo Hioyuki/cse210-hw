34d4eedf8b96c4377c20f6cada257d552ea70c46
Hioyuki <153518938+Hioyuki@users.noreply.github.com>
2024-04-28 14:38:04 +0900

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number =randomGenerator.Next(1,100);
+        
+        int response=0;
+
+        while (response != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            // response = Console.ReadLine();
+
+            response =int.Parse(Console.ReadLine());
+            if (number > response)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < response)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
+
 }
